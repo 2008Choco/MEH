3708b4bb7fa9eac09aa5cdd24fd1e61dab5bcc8d
2008Choco <hawkeboyz2@hotmail.com>
2024-10-02 20:05:04 -0400

    Abstract out manual GG into an AbstractMnemonicChatFeature

---
+++ src/main/java/wtf/choco/meh/client/chat/AbstractMnemonicChatFeature.java
@@ -0,0 +1,41 @@
+package wtf.choco.meh.client.chat;
+
+import java.util.function.Predicate;
+
+import net.minecraft.client.Minecraft;
+
+import wtf.choco.meh.client.MEHClient;
+import wtf.choco.meh.client.config.MEHConfig;
+import wtf.choco.meh.client.event.MEHEvents;
+import wtf.choco.meh.client.feature.Feature;
+import wtf.choco.meh.client.mnemonic.Mnemonic;
+
+abstract class AbstractMnemonicChatFeature extends Feature {
+
+    private final Mnemonic mnemonic;
+    private final String message;
+
+    public AbstractMnemonicChatFeature(MEHClient mod, Predicate<MEHConfig> featureEnabled, Mnemonic mnemonic, String message) {
+        super(mod, featureEnabled);
+        this.mnemonic = mnemonic;
+        this.message = message;
+    }
+
+    @Override
+    protected void registerListeners() {
+        MEHEvents.MNEMONIC_COMPLETION.register(this::onMnemonic);
+    }
+
+    private boolean onMnemonic(Mnemonic mnemonic) {
+        if (mnemonic != this.mnemonic || !isEnabled()) {
+            return true;
+        }
+
+        ChatChannelsFeature.dontSendToChannel = true; // "gg" should always be sent to the global chat channel!
+        Minecraft minecraft = Minecraft.getInstance();
+        minecraft.player.connection.sendChat(message);
+        ChatChannelsFeature.dontSendToChannel = false;
+        return true;
+    }
+
+}
--- src/main/java/wtf/choco/meh/client/chat/ManualGGFeature.java
+++ src/main/java/wtf/choco/meh/client/chat/ManualGGFeature.java
@@ -1,35 +1,13 @@
 package wtf.choco.meh.client.chat;
 
-import net.minecraft.client.Minecraft;
-
 import wtf.choco.meh.client.MEHClient;
 import wtf.choco.meh.client.config.MEHConfig;
-import wtf.choco.meh.client.event.MEHEvents;
-import wtf.choco.meh.client.feature.Feature;
-import wtf.choco.meh.client.mnemonic.Mnemonic;
 import wtf.choco.meh.client.mnemonic.Mnemonics;
 
-public final class ManualGGFeature extends Feature {
+public final class ManualGGFeature extends AbstractMnemonicChatFeature {
 
     public ManualGGFeature(MEHClient mod) {
-        super(mod, MEHConfig::isManualGGEnabled);
-    }
-
-    @Override
-    protected void registerListeners() {
-        MEHEvents.MNEMONIC_COMPLETION.register(this::onMnemonic);
-    }
-
-    private boolean onMnemonic(Mnemonic mnemonic) {
-        if (mnemonic != Mnemonics.GG || !isEnabled()) {
-            return true;
-        }
-
-        ChatChannelsFeature.dontSendToChannel = true; // "gg" should always be sent to the global chat channel!
-        Minecraft minecraft = Minecraft.getInstance();
-        minecraft.player.connection.sendChat("gg");
-        ChatChannelsFeature.dontSendToChannel = false;
-        return true;
+        super(mod, MEHConfig::isManualGGEnabled, Mnemonics.GG, "gg");
     }
 
 }
