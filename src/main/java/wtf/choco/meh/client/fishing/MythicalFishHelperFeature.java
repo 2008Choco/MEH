9c349ab20843008719eb6f2ea4b87ad2690e1985
2008Choco <hawkeboyz2@hotmail.com>
2025-07-20 19:48:45 -0400

    Support rendering in-world text as well. Cleanup code a little

--- src/main/java/wtf/choco/meh/client/fishing/MythicalFishHelperFeature.java
+++ src/main/java/wtf/choco/meh/client/fishing/MythicalFishHelperFeature.java
@@ -7,8 +7,12 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.phys.Vec3;
@@ -24,6 +28,8 @@ public final class MythicalFishHelperFeature extends Feature {
 
     private static final ResourceLocation TEXTURE_LOCATION = MEHClient.key("textures/wat.png");
 
+    private static final float GRAPHIC_DISTANCE_FROM_BOBBER = 1.0F;
+
     public MythicalFishHelperFeature(MEHClient mod) {
         super(mod);
     }
@@ -35,16 +41,10 @@ public final class MythicalFishHelperFeature extends Feature {
 
     @Override
     protected void registerListeners() {
-        WorldRenderEvents.AFTER_ENTITIES.register(this::renderHeatBar);
+        WorldRenderEvents.AFTER_ENTITIES.register(this::renderInWorldHelperGraphic);
     }
 
-    private void renderHeatBar(WorldRenderContext context) {
-        /*
-        MythicalFishEntity mythicalFish = MEHClient.getInstance().getHypixelServerState().getFishingState().getMythicalFishEntity();
-        if (mythicalFish == null) {
-            return;
-        }
-        */
+    private void renderInWorldHelperGraphic(WorldRenderContext context) {
         FishingHook fishingHook = MEHClient.getInstance().getHypixelServerState().getFishingState().getActiveFishingHook().get();
         if (fishingHook == null) {
             return;
@@ -56,24 +56,45 @@ public final class MythicalFishHelperFeature extends Feature {
             return;
         }
 
+        // Translate and rotate stack for world-anchored rendering
         Camera camera = context.camera();
         Vec3 cameraPos = camera.position();
         Vec3 targetPos = fishingHook.position();
 
         Vec3 targetToCamera = cameraPos.subtract(targetPos);
-        Vec3 perpendicularToUp = targetToCamera.cross(Direction.UP.getUnitVec3()).normalize().multiply(-1, -1, -1);
+        Vec3 perpendicularToUp = targetToCamera.cross(Direction.UP.getUnitVec3()).normalize().multiply(-GRAPHIC_DISTANCE_FROM_BOBBER, -GRAPHIC_DISTANCE_FROM_BOBBER, -GRAPHIC_DISTANCE_FROM_BOBBER);
 
         stack.pushPose();
-        stack.translate(targetPos.subtract(cameraPos).add(perpendicularToUp).add(0, 0.5, 0));
+        stack.translate(targetPos.subtract(cameraPos).add(perpendicularToUp));
         stack.mulPose(context.positionMatrix().invert(new Matrix4f()));
 
-        Pose pose = stack.last();
-        VertexConsumer buffer = consumers.getBuffer(MEHRenderTypes.worldPositionedGui(TEXTURE_LOCATION));
-        buffer.addVertex(pose, -0.5F, -0.5F, 0).setColor(0xFFFFFFFF).setUv(0, 1)
-            .addVertex(pose, 0.5F, -0.5F, 0).setColor(0xFFFFFFFF).setUv(1, 1)
-            .addVertex(pose, 0.5F, 0.5F, 0).setColor(0xFFFFFFFF).setUv(1, 0)
-            .addVertex(pose, -0.5F, 0.5F, 0).setColor(0xFFFFFFFF).setUv(0, 0);
+        this.renderHelperGraphic(stack, consumers);
+
+        // Translate stack for text rendering
+        Font font = Minecraft.getInstance().font;
+
+        stack.last().pose().rotate((float) Math.PI, 0.0F, 1.0F, 0.0F);
+        stack.scale(-0.025F, -0.025F, -0.025F);
+        stack.translate(0, -font.lineHeight + 1, 0);
+
+        this.renderHelperText(stack, consumers, font);
+
         stack.popPose();
+    }
+
+    private void renderHelperGraphic(PoseStack stack, MultiBufferSource bufferSource) {
+        VertexConsumer buffer = bufferSource.getBuffer(MEHRenderTypes.worldPositionedGui(TEXTURE_LOCATION));
+
+        Pose pose = stack.last();
+        buffer.addVertex(pose, 0F, 0F, 0).setColor(0xFFFFFFFF).setUv(0, 1)
+            .addVertex(pose, 1F, -0F, 0).setColor(0xFFFFFFFF).setUv(1, 1)
+            .addVertex(pose, 1F, 1F, 0).setColor(0xFFFFFFFF).setUv(1, 0)
+            .addVertex(pose, -0F, 1F, 0).setColor(0xFFFFFFFF).setUv(0, 0);
+    }
+
+    private void renderHelperText(PoseStack stack, MultiBufferSource bufferSource, Font font) {
+        Matrix4f pose = stack.last().pose();
+        font.drawInBatch(Component.literal("Example text!"), 0, 0, 0xFFFFFFFF, false, pose, bufferSource, Font.DisplayMode.NORMAL, 0, LightTexture.FULL_BRIGHT);
     }
 
 }
